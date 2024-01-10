277f27ab15bf812413bf39b288b90fc78dca2985
wzfh001 <142213472+wzfh001@users.noreply.github.com>
2024-01-10 13:38:48 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -30,8 +30,8 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-svn co https://github.com/kenzok8/small-package/trunk/luci-app-passwall
-svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-passwall
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-smartdns
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
